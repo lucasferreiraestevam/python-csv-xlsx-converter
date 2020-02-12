96c9e88dc04671db5fda5618c612412b497f0b6d
Lucas Ferreira <lucas@webbrains.com.br>
2020-02-12 18:22:49 -0300

    Init
    Init

---
+++ convert.py
@@ -0,0 +1,16 @@
+import sys
+import csv
+import os
+from openpyxl import Workbook
+
+files = os.listdir('CSV')
+for file in files:
+	root, ext = os.path.splitext(file)
+	if '.csv' in ext:
+		wb = Workbook()
+		ws = wb.active
+		with open('CSV/'+file) as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			for row in csv_reader:
+		    		ws.append(row)
+		wb.save('XLSX/'+root+'.xlsx')
